67f9b052470b4ef3cbe0918ef61d6f3d84f3ccc9
adamman722 <tarpeyadam@gmail.com>
2022-01-13 14:58:49 -0500

    Finished product

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Heading from "./Heading.jsx";
+import List from "./Unorderedlist";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <List />
+    </div>
+  );
+}
+
+export default App;
